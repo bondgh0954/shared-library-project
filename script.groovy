7d98650a9cd1cd9fd1a442eaf7a4e51d6e11680f
bondgh0954 <otengdevops@gmail.com>
2024-06-09 23:49:09 +0200

    modify groovy file

--- script.groovy
+++ script.groovy
@@ -1,14 +1,4 @@
-def buildJar(){
-    echo 'building jar file from the application'
-    sh 'mvn package'
-}
 
-def buildImage(){
-    echo 'building docker image from the application'
-    withCredentials([usernamePassword(credentialsId:'dockerhub-credentials',usernameVariable:'USER',passwordVariable:'PASSWORD')]){
-        sh "docker build -t nanaot/java-app:5.4 ."
-        sh "echo $PASSWORD | docker login -u $USER --password-stdin"
-        sh "docker push nanaot/java-app:5.4"
-    }
-}
+
+
 return this
